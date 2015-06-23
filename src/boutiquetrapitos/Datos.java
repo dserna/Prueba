1aa10a655da60ab2f7763c0207844cc141e6d323
user <user@user-PC>
2015-06-23 11:55:19 -0500

    Cambio de usuario..

--- src/boutiquetrapitos/Datos.java
+++ src/boutiquetrapitos/Datos.java
@@ -6,7 +6,7 @@ package boutiquetrapitos;
 public class Datos {
    
     public boolean validarIngreso(String usuario, String contraseña){
-        if(usuario.equals("guzman") && contraseña.equals("123")){
+        if(usuario.equals("sa") && contraseña.equals("123")){
             return true;
         }
         else{
