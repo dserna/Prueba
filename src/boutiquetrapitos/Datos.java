d1e92c8ec30fb58062546a6d812cfd13e1c27506
user <user@user-PC>
2015-06-23 12:17:58 -0500

    Documentacion de la clase datos.

--- src/boutiquetrapitos/Datos.java
+++ src/boutiquetrapitos/Datos.java
@@ -1,10 +1,18 @@
 
 package boutiquetrapitos;
 
-
+/**
+* Constructor de la clase datos
+* @author Daniel Serna
+*/
 
 public class Datos {
-   
+   /**
+    * Metodo Booleano para validar usuario
+    * @param usuario
+    * @param contraseña
+    * @return true = Verdadero e ingresa al aplicativo.
+    */
     public boolean validarIngreso(String usuario, String contraseña){
         if(usuario.equals("sa") && contraseña.equals("123")){
             return true;
@@ -12,7 +20,5 @@ public class Datos {
         else{
             return false;
         }
-    }
-    
-    
-}
+    } // Cierre del metodo. 
+} // Cierre del constructor.
